8e7d9a4acea99f0d275a6e226460fd39efc5e1b3
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2022-06-14 21:00:08 -0300

    feat: database communications for writing data into db

--- testglossary/database/db_instance.py
+++ testglossary/database/db_instance.py
@@ -1,11 +1,10 @@
-
 # Database Dependency
 from testglossary.database import connection
 
 
 def use_db():
-    """"Generate an instance of database connection to be used."""
-    
+    """ "Generate an instance of database connection to be used."""
+
     db = connection.SessionLocal()
     try:
         yield db
--- testglossary/database/interface.py
+++ testglossary/database/interface.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 
 from testglossary.database.entities import Term
+from testglossary.models.schemas import CreateTerm
 
 
 def get_terms(db: Session, page: int = 0, results_per_page: int = 30):
@@ -23,3 +24,20 @@ def get_term_by_name(
         .limit(results_per_page)
         .all()
     )
+
+
+def register_new_term(db: Session, term_data: CreateTerm):
+    """
+    Register a test term into database
+    """
+    db_new_term = Term(
+        name=term_data.name,
+        definition=term_data.definition,
+        acronym=term_data.acronym,
+        version=term_data.version,
+        language_id=term_data.language_id,
+    )
+    db.add(db_new_term)
+    db.commit()
+    db.refresh(db_new_term)
+    return db_new_term
